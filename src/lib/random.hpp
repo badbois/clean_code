8b35ee1e154d2fe0c531f003c2d9662440979175
Roberty Jordan <jordan.roberty@outlook.fr>
2022-03-02 17:52:45 +0100

    [lib] Moving random into lib dir and transform it into template

--- src/lib/random.hpp
+++ src/lib/random.hpp
@@ -3,8 +3,8 @@
 #include <random>
 
 /// Returns a random int between min (included) and max (included)
-inline int rand(int min, int max) {
+template <typename T> inline T rand(T min, T max) {
   static std::default_random_engine generator{std::random_device{}()};
-  std::uniform_int_distribution<int> distribution{min, max};
+  std::uniform_int_distribution<T> distribution{min, max};
   return distribution(generator);
 }
